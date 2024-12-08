e391275ebc1bf4c59b81cef18925b8f0f8b7f9a9
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2024-12-08 17:54:20 +0900

    Revert "伸ばしてみる"
    This reverts commit 7e7cf12885093364ca0aa27fc2be37bd32997df8.

--- go/chair_handlers.go
+++ go/chair_handlers.go
@@ -24,7 +24,7 @@ type chairPostChairsResponse struct {
 	OwnerID string `json:"owner_id"`
 }
 
-const RetryAfterMs = 2500
+const RetryAfterMs = 1500
 
 func chairPostChairs(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
