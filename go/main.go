281c394ed3727277d576548fafc67f9f6de219e7
sh0n0 <44708388+sh0n0@users.noreply.github.com>
2024-12-08 12:06:28 +0900

    interpolateParams=true

--- go/main.go
+++ go/main.go
@@ -65,6 +65,7 @@ func setup() http.Handler {
 	dbConfig.Net = "tcp"
 	dbConfig.DBName = dbname
 	dbConfig.ParseTime = true
+	dbConfig.InterpolateParams = true
 
 	_db, err := sqlx.Connect("mysql", dbConfig.FormatDSN())
 	if err != nil {
